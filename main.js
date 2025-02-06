2f9061d6c76c96d6a8958667068d779dd3f55d33
Alief <etaanomore@gmail.com>
2025-02-06 19:35:54 +0700

    new

--- main.js
+++ main.js
@@ -42,22 +42,79 @@
 //     alert(`anda salah. Hari ini adalah ${hariIni}`)
 // }
 
-const Bulan = new Date().getMonth()
-let bulanIni
-if (Bulan == 0) {
-    bulanIni = "januari"
-}else if (Bulan == 1) {
-    bulanIni = "februari"
-}else if (Bulan == 2) {
-    bulanIni = "maret"
-}else if (Bulan == 3) {
-    bulanIni = "april"
-}else{
-    bulanIni = "bulan apa ya?"
+// const Bulan = new Date().getMonth()
+// let bulanIni
+// if (Bulan == 0) {
+//     bulanIni = "januari"
+// }else if (Bulan == 1) {
+//     bulanIni = "februari"
+// }else if (Bulan == 2) {
+//     bulanIni = "maret"
+// }else if (Bulan == 3) {
+//     bulanIni = "april"
+// }else{
+//     bulanIni = "bulan apa ya?"
+// }
+// const inputUser = prompt('bulan apa sekarang?')
+// if (inputUser == bulanIni){
+//     alert('anda pintar')
+// }else{
+//     alert(`anda goblok, bulan ini adalah ${bulanIni}`)
+// }
+
+document.title = 'Alief Albayu'
+const body = document.body
+const btn1 = document.getElementById('btn1')
+const btn2 = document.getElementById('btn2')
+
+// const defaultText = 'KLIK SAYA 1'
+// btn1.textContent = defaultText
+
+
+// btn1.style.border = 'none'
+// btn1.style.padding = '5px'
+// btn1.style.fontSize = '20px'
+// btn1.style.background = 'salmon'
+
+// function clickButton1() {
+//     btn1.style.background = 'red'
+//     const newText = document.createElement ('p')
+//     newText.textContent = 'Halo Bay'
+//     body.append(newText)
+// }
+
+// function ubahText1() {
+//     btn1.textContent = 'SUDAH DI KLIK'
+// }
+
+// function backText1() {
+//     btn1.textContent = defaultText
+// }
+
+const defaultText = 'KLIK SAYA 1'
+btn2.textContent = defaultText
+
+btn2.style.border = 'none'
+btn2.style.padding = '5px'
+btn2.style.fontSize = '20px'
+btn2.style.background = 'salmon'
+
+function clickButton2() {
+    btn2.style.background = 'red'
+    const newText = document.createElement ('p')
+    newText.textContent = 'Halo Bay'
+    body.append(newText)
 }
-const inputUser = prompt('bulan apa sekarang?')
-if (inputUser == bulanIni){
-    alert('anda pintar')
-}else{
-    alert(`anda goblok, bulan ini adalah ${bulanIni}`)
+
+function ubahText2() {
+    btn2.textContent = 'SUDAH DI KLIK'
+    const newText = document.createElement ('p')
+    newText.textContent = 'halo pre'
+    body.append(newText)
+}
+
+function backText2() {
+    const changeBg = document.querySelector('p')
+    changeBg.style.color = 'blue'
+    console.log("perubahan ke:")
 }
